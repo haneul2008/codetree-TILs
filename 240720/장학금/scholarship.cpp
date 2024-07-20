f832a116f8d8566d82108c3189f739ac33d0f08e
haneul2008 <157570674+haneul2008@users.noreply.github.com>
2024-07-20 19:00:00 +0900

    Title: 장학금, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240720/장학금/scholarship.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int s1, s2;
+    cin >> s1 >> s2;
+
+    if(s1 <= 90){
+        cout << 0;
+    }
+    else if(s2 >= 95){
+        cout << 100000;
+    }
+    else if(s2 >= 90){
+        cout << 50000;
+    }
+    return 0;
+}
