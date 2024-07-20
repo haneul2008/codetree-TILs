0fa8e9236be711ab60a3b1f4e71d088ee7fd02f5
haneul2008 <157570674+haneul2008@users.noreply.github.com>
2024-07-20 18:27:25 +0900

    Title: 4가지 관계연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240720/4가지 관계연산자/four-relational-operators.cpp
+++ 240720/4가지 관계연산자/four-relational-operators.cpp
@@ -7,7 +7,7 @@ int main() {
     cin >> a >> b;
 
     cout << (a >= b) << endl;
-    cout << (a == b) << endl;
+    cout << (a > b) << endl;
     cout << (a <= b) << endl;
     cout << (a < b) << endl;
     return 0;
