5fc37a58bd7d5d1b3547a8b33f839b72c398a0af
haneul2008 <157570674+haneul2008@users.noreply.github.com>
2024-07-21 00:31:50 +0900

    Title: 369 게임, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240721/369 게임/369-game.cpp
+++ 240721/369 게임/369-game.cpp
@@ -7,12 +7,14 @@ int main() {
     cin >> n;
 
     for(int i = 1; i <= n; i++){
-        if(i % 3 == 0){
-            cout << 0 << ' ';
-        }
-        else{
-            cout << i << ' ';
-        }
+        if(i % 3 == 0)
+			cout << "0 ";
+		else if(i % 10 == 3 || i % 10 == 6 || i % 10 == 9)
+			cout << "0 ";
+		else if(i / 10 == 3 || i / 10 == 6 || i / 10 == 9)
+			cout << "0 ";
+		else
+			cout << i << " ";
     }
     return 0;
 }
