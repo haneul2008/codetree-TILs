a655937339e29c3dbc934bd006341c450f740364
haneul2008 <157570674+haneul2008@users.noreply.github.com>
2024-07-20 18:32:20 +0900

    Title: 점수 비교, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240720/점수 비교/score-comparison.cpp
+++ 240720/점수 비교/score-comparison.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int mA, mB, eA, eB;
+    int mA, eA, mB, eB;
     cin >> mA >> mB >> eA >> eB;
 
-    cout << (mA > mB && eA > eB);
+    cout << ((mA > mB) && (eA > eB));
     return 0;
 }
