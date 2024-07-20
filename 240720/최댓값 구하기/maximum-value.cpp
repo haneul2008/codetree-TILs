9a274e300ddc05837d2d4fd2c2c6372377c8f80a
haneul2008 <157570674+haneul2008@users.noreply.github.com>
2024-07-20 19:22:53 +0900

    Title: 최댓값 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240720/최댓값 구하기/maximum-value.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int max = 0;
+
+    if(max < a)
+        max = a;
+    if(max < b)
+        max = b;
+    if(max < c)
+        max = c;
+
+    cout << max;
+    return 0;
+}
